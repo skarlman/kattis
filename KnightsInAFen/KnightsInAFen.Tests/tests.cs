2b94a57974d62c30be60ccd7c1183d6b23e50b01
Karl T <karl.tillstrom@gmail.com>
2017-07-07 15:08:54 +0200

    Initial commit

---
+++ KnightsInAFen/KnightsInAFen.Tests/tests.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace KnightsInAFen.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+
+        [TestCase("00100,10110,01 11,10111,01001", 2, 2)]
+        [TestCase(" 0100,10110,00011,10111,01001", 0, 0)]
+        [TestCase("10100,10110,00011,10111,0101 ", 4, 4)]
+        [TestCase("10100,101 0,00011,10111,01011", 1, 3)]
+        public void FindEmptySquare(string boardString, int expectedRow, int expectedColumn)
+        {
+            var board = boardString.Split(',').ToList();
+
+            var result = KnightsInAFen.Program.FindEmptySquare(board);
+
+            Assert.AreEqual(expectedRow, result.Row);
+            Assert.AreEqual(expectedColumn, result.Column);
+        }
+
+
+        [Test]
+        public void CanDetectCompleteBoard()
+        {
+            var board = new List<string>()
+            {
+                "11111","01111","00 11","00001","00000"
+            };
+
+            var result = KnightsInAFen.Program.NumberOfMisplacedPieces(board);
+
+            Assert.AreEqual(0,result);
+        }
+
+        [Test]
+        public void CanDetectInCompleteBoard()
+        {
+            var board = new List<string>()
+            {
+                "11111","10111","00 11","00001","00000"
+            };
+
+            var result = KnightsInAFen.Program.NumberOfMisplacedPieces(board);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void GetAllPossibleMoves()
+        {
+            //var board = new List<string>() {"00100",
+            //                                "10110",
+            //                                "01 11",
+            //                                "10111",
+            //                                "01001" };
+
+            List<KnightsInAFen.Program.Position> result =
+                KnightsInAFen.Program.GetAllPossibleMoves(new KnightsInAFen.Program.Position(2, 2));
+
+            List<KnightsInAFen.Program.Position> expected = new List<KnightsInAFen.Program.Position>()
+            {
+                new KnightsInAFen.Program.Position(0, 1),
+                new KnightsInAFen.Program.Position(0, 3),
+                new KnightsInAFen.Program.Position(1, 0),
+                new KnightsInAFen.Program.Position(1, 4),
+                new KnightsInAFen.Program.Position(3, 0),
+                new KnightsInAFen.Program.Position(3, 4),
+                new KnightsInAFen.Program.Position(4, 1),
+                new KnightsInAFen.Program.Position(4, 3)
+            };
+
+            CollectionAssert.AllItemsAreUnique(result);
+            Assert.AreEqual(expected.Count, result.Count);
+            foreach (var position in result)
+            {
+                Assert.IsTrue(expected.Any(e => e.Row == position.Row && e.Column == position.Column));
+            }
+            //CollectionAssert.AreEquivalent(expected,result);
+        }
+
+
+        [Test]
+        public void GivenTestcases()
+        {
+            var input = @"01011
+110 1
+01110
+01010
+00100
+10110
+01 11
+10111
+01001
+00000".Split(Environment.NewLine.ToCharArray()).Where(s => !string.IsNullOrEmpty(s)).ToList();
+
+            var result = KnightsInAFen.Program.CalculateMoves(input);
+
+            Assert.AreEqual(11, result[0]);
+            Assert.AreEqual(7, result[1]);
+            //Assert.AreEqual("Unsolvable in less than 11 move(s).", result[0]);
+            //Assert.AreEqual("Solvable in 7 move(s).", result[1]);
+        }
+    }
+}
---
+++ KnightsInAFen/KnightsInAFen/Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KnightsInAFen
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberOfBoards = int.Parse(Console.ReadLine());
+            var rawBoards = new List<string>(numberOfBoards*5);
+
+            for (int i = 0; i < numberOfBoards*5; i++)
+            {
+                rawBoards.Add(Console.ReadLine());
+            }
+
+            var result = CalculateMoves(rawBoards);
+
+            result.ForEach(r =>
+            {
+                if (r <= 10)
+                    Console.WriteLine("Solvable in {0} move(s).", r);
+                else
+                    Console.WriteLine("Unsolvable in less than 11 move(s).");
+            });
+        }
+
+        public static List<int> CalculateMoves(List<string> input)
+        {
+            var result = new List<int>();
+            var numberOfBoards = input.Count/5;
+
+            for (int i = 0; i < numberOfBoards; i++)
+            {
+                var currentBoard = input.Skip(i*5).Take(5).ToList();
+                result.Add(SolveBoard(currentBoard));
+            }
+
+            return result;
+        }
+
+        private static int SolveBoard(List<string> board)
+        {
+            var boardsToExplore = new Queue<BoardWithLevel>();
+
+            var startingBoard = new BoardWithLevel(board, FindEmptySquare(board));
+            boardsToExplore.Enqueue(startingBoard);
+
+            var seenBoards = new Dictionary<string, bool>();
+
+            while (boardsToExplore.Count > 0)
+            {
+                var currentBoard = boardsToExplore.Dequeue();
+
+                if (BoardIsComplete(currentBoard))
+                {
+                    return currentBoard.Level;
+                }
+
+                var currentBoardKey = string.Join("",currentBoard);
+
+                if (seenBoards.ContainsKey(currentBoardKey))
+                {
+                    continue;
+                }
+
+                seenBoards.Add(currentBoardKey, true);
+
+                var numberOfMisplacedPieces = NumberOfMisplacedPieces(currentBoard);
+                var minimumTotalMoves = (numberOfMisplacedPieces-1)+currentBoard.Level;
+
+                if (currentBoard.Level < 10 && minimumTotalMoves < 11)
+                {
+                    var emptySquare = currentBoard.EmptySquarePosition;
+                    var possibleMoves = GetAllPossibleMoves(emptySquare);
+
+                    foreach (var move in possibleMoves)
+                    {
+                        var newBoard = CreateUpdatedBoard(currentBoard, emptySquare, move);
+                        var newBoardKey = string.Join("", newBoard);
+
+                        if (!seenBoards.ContainsKey(newBoardKey))
+                        {
+                            boardsToExplore.Enqueue(new BoardWithLevel(newBoard.ToList(), move, currentBoard.Level + 1));
+                        }
+                    }
+                }
+            }
+            return 11;
+        }
+
+        private static string[] CreateUpdatedBoard(BoardWithLevel currentBoard, Position emptySquare, Position move)
+        {
+            var newBoard = new string[5];
+            currentBoard.CopyTo(newBoard);
+
+            var theRow = newBoard[emptySquare.Row].ToCharArray();
+            theRow[emptySquare.Column] = newBoard[move.Row][move.Column];
+            newBoard[emptySquare.Row] = new string(theRow);
+
+            theRow = newBoard[move.Row].ToCharArray();
+            theRow[move.Column] = ' ';
+            newBoard[move.Row] = new string(theRow);
+
+            return newBoard;
+        }
+
+
+        static readonly string[] CompletedBoard = new string[5]
+        {
+                        "11111",
+                        "01111",
+                        "00 11",
+                        "00001",
+                        "00000"
+        };
+
+        public static int NumberOfMisplacedPieces(List<string> board)
+        {
+            var counter = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (board[i][j] != CompletedBoard[i][j])
+                    {
+                        counter++;
+                    }
+                }
+            }
+
+            return counter;
+        }
+        public static bool BoardIsComplete(List<string> board)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (board[i][j] != CompletedBoard[i][j])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public static Position FindEmptySquare(List<string> currentBoard)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (currentBoard[i][j] == ' ')
+                    {
+                        return new Position(i, j);
+                    }
+                }
+            }
+            return null;
+        }
+
+        public class Position 
+        {
+            public Position(int row, int column)
+            {
+                Row = row;
+                Column = column;
+            }
+
+            public int Row { get; set; }
+            public int Column { get; set; }
+        }
+
+        public class BoardWithLevel : List<string>
+        {
+            public int Level { get; set; }
+
+            public Position EmptySquarePosition { get; set; }
+
+            public BoardWithLevel(List<string> collection, Position emptySquare, int level=0) : base(collection)
+            {
+                this.Level = level;
+                this.EmptySquarePosition = emptySquare;
+            }
+        }
+
+        public static List<Position> GetAllPossibleMoves(Position emptySquare)
+        {
+            var result = new List<Position>();
+
+            result.Add(new Position(emptySquare.Row-2, emptySquare.Column-1));
+            result.Add(new Position(emptySquare.Row-2, emptySquare.Column+1));
+            result.Add(new Position(emptySquare.Row+2, emptySquare.Column-1));
+            result.Add(new Position(emptySquare.Row+2, emptySquare.Column+1));
+
+            result.Add(new Position(emptySquare.Row-1, emptySquare.Column-2));
+            result.Add(new Position(emptySquare.Row-1, emptySquare.Column+2));
+            result.Add(new Position(emptySquare.Row+1, emptySquare.Column-2));
+            result.Add(new Position(emptySquare.Row+1, emptySquare.Column+2));
+
+            return result.Where(p => p.Row >= 0 && p.Column >= 0 && p.Row < 5 && p.Column < 5).ToList();
+        }
+    }
+}
